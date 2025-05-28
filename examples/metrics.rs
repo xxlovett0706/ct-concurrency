2f2f0b8411ec7d6dec0d0f2ccd3665a56c76b3d3
xxlovett0706 <xxlovett0706@gmail.com>
2025-05-28 13:07:52 +0800

    feature: support concurrent metrices

---
+++ examples/metrics.rs
@@ -0,0 +1,46 @@
+use std::{thread, time::Duration};
+
+use concurrency::Metrics;
+use rand::Rng;
+
+const N: usize = 2;
+const M: usize = 4;
+
+fn main() {
+    let metrics = Metrics::new();
+
+    // start N workers and M requesters
+    for i in 0..N {
+        task_worker(i, metrics.clone());
+    }
+    for _ in 0..M {
+        request_worker(metrics.clone());
+    }
+
+    loop {
+        thread::sleep(Duration::from_millis(5000));
+        println!("{:?}", metrics.snapshot());
+    }
+}
+
+fn task_worker(idx: usize, metrics: Metrics) {
+    thread::spawn(move || loop {
+        let mut rng = rand::rng();
+        thread::sleep(Duration::from_millis(rng.random_range(100..=5000)));
+        if let Err(e) = metrics.inc(format!("call.thread.worker.{}", idx)) {
+            eprintln!("Error incrementing metrics: {}", e);
+        }
+    });
+}
+
+fn request_worker(metrics: Metrics) {
+    thread::spawn(move || loop {
+        let mut rng = rand::rng();
+        thread::sleep(Duration::from_millis(rng.random_range(50..=800)));
+
+        let page = rng.random_range(1..256);
+        if let Err(e) = metrics.inc(format!("req.page.{}", page)) {
+            eprintln!("Error incrementing metrics: {}", e);
+        }
+    });
+}
